fdb13030f0dfc676397841df513ef8b072ef2cc1
antipenko <sweetglory.victory@gmail.com>
2016-09-21 07:37:31 +0300

    add all changes

--- app/js/app.js
+++ app/js/app.js
@@ -1,2 +1,14 @@
 $(document).foundation();
+$(document).ready(function(){
+  $('.sh-special-list').slick({
+    dots:true,
+    arrows: false
+  });
+});
 
+$('.btn-search').on('click', function(){
+	console.log('cl');
+	if ($('#search').is(':hidden')){
+
+	}
+});
